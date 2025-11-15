3ae44c67458751a230f8fc1a1965df5734336c81
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2025-11-15 14:12:31 -0500

    chore: Remove pointless buffer check

--- server/lib/media/image/ImageAdjust.ts
+++ server/lib/media/image/ImageAdjust.ts
@@ -13,13 +13,10 @@ type ImageOptions = {
  * @returns resized/compressed image buffer
  */
 export const adjustImage = async (
-  data: Buffer,
+  buffer: Buffer,
   options: ImageOptions,
 ): Promise<Buffer> => {
   const { size, quality = 100 } = options;
-
-  // Accept either a Buffer or base64 string
-  const buffer = Buffer.isBuffer(data) ? data : Buffer.from(data, "base64");
 
   // Use sharp for resizing and quality adjustment
   return sharp(buffer).resize(size).jpeg({ quality }).toBuffer();
