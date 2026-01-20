755cfb14c56b70aa73346bac765dd1bf527eaac7
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2026-01-19 20:56:05 -0500

    fix: Disable shuffle on random play

--- client/components/App/PlayControls/Shuffle.tsx
+++ client/components/App/PlayControls/Shuffle.tsx
@@ -3,15 +3,19 @@ import { getPlayerState } from "@reducers/store";
 
 import { ShuffleIcon } from "@client/components/ui/icons/ShuffleIcon";
 import { useShuffle } from "@hooks/index";
+import { PlayMode } from "@common/types";
 
 export const Shuffle = () => {
   const player = useSelector(getPlayerState);
   const shuffle = useShuffle();
+  const disabled = player.play_options.mode === PlayMode.Random;
+
+  const className =
+    "icon-xs" +
+    (disabled ? " disabled" : player.is_shuffled ? " active" : "") +
+    "";
 
   return (
-    <ShuffleIcon
-      onClick={shuffle}
-      className={player.is_shuffled ? "active icon-xs" : "icon-xs"}
-    />
+    <ShuffleIcon onClick={() => !disabled && shuffle()} className={className} />
   );
 };
