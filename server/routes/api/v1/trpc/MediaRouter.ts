ede86fddb1601a3589cd0ce034ab359d58883786
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2026-02-07 12:06:28 -0500

    fix: Require admin for scan

--- server/routes/api/v1/trpc/MediaRouter.ts
+++ server/routes/api/v1/trpc/MediaRouter.ts
@@ -1,9 +1,9 @@
 import { MediaFileController, schema } from "@controllers/MediaFileController";
-import { procedure, router } from "@server/trpc";
+import { admin_procedure, procedure, router } from "@server/trpc";
 
 export const MediaRouter = (routes: typeof router) =>
   routes({
-    scan: procedure.mutation(MediaFileController.scan),
+    scan: admin_procedure.mutation(MediaFileController.scan),
     search: procedure.input(schema.search).query(MediaFileController.search),
     byFileType: procedure.query(MediaFileController.byFileType),
   });
